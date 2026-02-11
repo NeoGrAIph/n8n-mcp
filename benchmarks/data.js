fc5347c7ebc603f9464364fe7ef57e1c7375230b
github-action-benchmark <github@users.noreply.github.com>
2026-02-11 03:34:37 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 613f45937f8d535ccfba09b4a3be014fba5b5360

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770732684727,
+  "lastUpdate": 1770780877848,
   "repoUrl": "https://github.com/NeoGrAIph/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -23,6 +23,37 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/NeoGrAIph/n8n-mcp/commit/7c9b15e57d77e3ca0b7435d5e5d35b31235e247d"
         },
         "date": 1770732684408,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "neograiph@gmail.com",
+            "name": "Nikolay Satsuk",
+            "username": "NeoGrAIph"
+          },
+          "committer": {
+            "email": "neograiph@gmail.com",
+            "name": "Nikolay Satsuk",
+            "username": "NeoGrAIph"
+          },
+          "distinct": true,
+          "id": "613f45937f8d535ccfba09b4a3be014fba5b5360",
+          "message": "feat(workflow-files): expose relativePath and display path for code/set files",
+          "timestamp": "2026-02-11T11:32:32+08:00",
+          "tree_id": "ab59b18e57c47b2fb9352de54d52c3b951aa653f",
+          "url": "https://github.com/NeoGrAIph/n8n-mcp/commit/613f45937f8d535ccfba09b4a3be014fba5b5360"
+        },
+        "date": 1770780877456,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
