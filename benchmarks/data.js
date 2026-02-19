801eff32b498d9e4566e358bd5033148d08e8522
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 08:22:52 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for c466d025ff52598b3b4adfa9d1dd1514773b8fb0

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770780877848,
+  "lastUpdate": 1771489372539,
   "repoUrl": "https://github.com/NeoGrAIph/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -54,6 +54,37 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/NeoGrAIph/n8n-mcp/commit/613f45937f8d535ccfba09b4a3be014fba5b5360"
         },
         "date": 1770780877456,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "neograiph@gmail.com",
+            "name": "Nikolay Satsuk",
+            "username": "NeoGrAIph"
+          },
+          "committer": {
+            "email": "neograiph@gmail.com",
+            "name": "Nikolay Satsuk",
+            "username": "NeoGrAIph"
+          },
+          "distinct": true,
+          "id": "c466d025ff52598b3b4adfa9d1dd1514773b8fb0",
+          "message": "fix MCP schema arrays and stabilize unit tests",
+          "timestamp": "2026-02-19T16:20:32+08:00",
+          "tree_id": "ae31ff42e858c90b05af71abfa4306f98f650307",
+          "url": "https://github.com/NeoGrAIph/n8n-mcp/commit/c466d025ff52598b3b4adfa9d1dd1514773b8fb0"
+        },
+        "date": 1771489372254,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
